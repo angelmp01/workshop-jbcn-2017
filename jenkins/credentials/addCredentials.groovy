ed03c400f64ff9085038cdeb8719fc1ff7f8e24d
Carlos María Cornejo Crespo <cmcornejo@atsistemas.com>
2017-06-15 18:00:09 +0200

    added sudo package to Jenkins image

--- jenkins/credentials/addCredentials.groovy
+++ jenkins/credentials/addCredentials.groovy
@@ -2,6 +2,8 @@ import com.cloudbees.plugins.credentials.impl.*;
 import com.cloudbees.plugins.credentials.*;
 import com.cloudbees.plugins.credentials.domains.*;
 
-Credentials c = (Credentials) new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL,"gitlab", "Gitlab Credentials", "root", "jBCNConf2017")
+Credentials gitlabCreds = (Credentials) new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL,"gitlab", "Gitlab Credentials", "root", "jBCNConf2017")
+Credentials dockerRegistryCreds = (Credentials) new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL,"registryCreds", "Docker Registry Credentials", "admin", "admin")
 
-SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), c)
+SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), gitlabCreds)
+SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), dockerRegistryCreds)
