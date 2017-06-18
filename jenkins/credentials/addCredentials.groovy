95c7074980754ae3be4c6ef83646dc1fad5fdc07
Carlos M. Cornejo <carlos.cornejo.crespo@gmail.com>
2017-06-18 22:41:35 +0200

    removed non-used reggsitry creds Changed gitlab config to improve performance. Added extra dangled volumes housekeeping

--- jenkins/credentials/addCredentials.groovy
+++ jenkins/credentials/addCredentials.groovy
@@ -3,7 +3,5 @@ import com.cloudbees.plugins.credentials.*;
 import com.cloudbees.plugins.credentials.domains.*;
 
 Credentials gitlabCreds = (Credentials) new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL,"gitlab", "Gitlab Credentials", "root", "jBCNConf2017")
-Credentials dockerRegistryCreds = (Credentials) new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL,"registryCreds", "Docker Registry Credentials", "admin", "admin")
 
 SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), gitlabCreds)
-SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), dockerRegistryCreds)
